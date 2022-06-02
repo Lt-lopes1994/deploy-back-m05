b48bce2c0fe514fc5eb3fd70e3862d45e735ae7b
Bruno Mantovan Lopes <brunomantovanlopes@gmail.com>
2022-06-02 17:12:36 -0300

    Tirando validação de token para ambiente de testes

--- src/routes.js
+++ src/routes.js
@@ -1,48 +1,52 @@
-const express = require('express');
-const { registerUser,
-    informationToTheUserHimself,
-    updateUser } = require('./controllers/users');
-const login = require('./controllers/login');
-const { registerCustomer,
-    delinquentCustomerHighligths,
-    highlightsCustomersUpToDate,
-    customers,
-    customerDetail } = require('./controllers/customers');
-const { chargesPaid,
-    overdueCharges,
-    anticipatedCharges,
-    highlightsOverdueCollections,
-    highlightsExpectedCharges,
-    highlightsPaidCharges } = require('./controllers/charges');
+const express = require("express");
+const {
+  registerUser,
+  informationToTheUserHimself,
+  updateUser,
+} = require("./controllers/users");
+const login = require("./controllers/login");
+const {
+  registerCustomer,
+  delinquentCustomerHighligths,
+  highlightsCustomersUpToDate,
+  customers,
+  customerDetail,
+} = require("./controllers/customers");
+const {
+  chargesPaid,
+  overdueCharges,
+  anticipatedCharges,
+  highlightsOverdueCollections,
+  highlightsExpectedCharges,
+  highlightsPaidCharges,
+} = require("./controllers/charges");
 
-const checkLogin = require('./middlewares/tokenNeeded');
+const checkLogin = require("./middlewares/tokenNeeded");
 
 const routes = express();
 
-routes.post('/signup', registerUser);
-routes.post('/login', login);
+routes.post("/signup", registerUser);
+routes.post("/login", login);
 
-routes.use(checkLogin);
+// routes.use(checkLogin);
 
-routes.patch('/updateUser', updateUser);
-routes.get('/informationToTheUserHimself', informationToTheUserHimself)
+routes.patch("/updateUser", updateUser);
+routes.get("/informationToTheUserHimself", informationToTheUserHimself);
 
-routes.post('/registerCustomer', registerCustomer);
-routes.get('/customers', customers);
+routes.post("/registerCustomer", registerCustomer);
+routes.get("/customers", customers);
 
 // routes.get('/customerDetail', customerDetail);
 
-routes.get('/chargesPaid', chargesPaid);
-routes.get('/overdueCharges', overdueCharges);
-routes.get('/anticipatedCharges', anticipatedCharges);
+routes.get("/chargesPaid", chargesPaid);
+routes.get("/overdueCharges", overdueCharges);
+routes.get("/anticipatedCharges", anticipatedCharges);
 
-routes.get('/highlightsOverdueCollections', highlightsOverdueCollections);
-routes.get('/highlightsExpectedCharges', highlightsExpectedCharges);
-routes.get('/highlightsPaidCharges', highlightsPaidCharges);
+routes.get("/highlightsOverdueCollections", highlightsOverdueCollections);
+routes.get("/highlightsExpectedCharges", highlightsExpectedCharges);
+routes.get("/highlightsPaidCharges", highlightsPaidCharges);
 
-routes.get('/delinquentCustomerHighligths', delinquentCustomerHighligths);
-routes.get('/highlightsCustomersUpToDate', highlightsCustomersUpToDate);
-
-
+routes.get("/delinquentCustomerHighligths", delinquentCustomerHighligths);
+routes.get("/highlightsCustomersUpToDate", highlightsCustomersUpToDate);
 
 module.exports = routes;
