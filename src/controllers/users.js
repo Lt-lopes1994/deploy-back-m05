12848e2e2b2c4d27b2393d442bdbe788ac5b5b94
unknown <nathan.renovato300@gmail.com>
2022-06-20 20:07:04 -0300

    M

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -27,100 +27,103 @@ const registerUser = async (req, res) => {
 
     const addUser = await knex('users')
       .insert({ name, email, password: hash });
+  } catch {
+    return res.status(400).json(error.message);
+  }
+}
 
+const informationToTheUserHimself = async (req, res) => {
+  return res.status(200).json(req.user);
+};
 
-    const informationToTheUserHimself = async (req, res) => {
-      return res.status(200).json(req.user);
-    };
-
-    /*const updateUser = async (req, res) => {
-      const { id } = req.user;
-      const { name, cpf, email, phone, password } = req.body;
-    
-      try {
-        const user = await knex("users")
-          .select("id", "name", "email", "cpf", "phone", "password")
-          .where(id)
-          .first();
-    
-            if (!addUser) {
-                return res.status(400).json(errors.couldNotSignin);
-            }
-    
-            return res.status(201).json();
-        } catch (error) {
-            return res.status(400).json(error.message);
+/*const updateUser = async (req, res) => {
+  const { id } = req.user;
+  const { name, cpf, email, phone, password } = req.body;
+ 
+  try {
+    const user = await knex("users")
+      .select("id", "name", "email", "cpf", "phone", "password")
+      .where(id)
+      .first();
+ 
+        if (!addUser) {
+            return res.status(400).json(errors.couldNotSignin);
         }
-    };
-    */
-
-
-    const updateUser = async (req, res) => {
-      const { id } = req.user;
-      let { name, email, password, cpf, phone } = req.body;
-
-      try {
-        await userSchema.editUser.validate(req.body);
-        const getUser = await usersTemplate.userExists(id);
-        if (!getUser) {
-          return res.status(404).json(errors.userNotFound);
-        }
-
-        if (password) {
-          password = await bcrypt.hash(password.trim(), 10);
-        }
-
-        if (email && email !== req.user.email) {
-          const getEmail = await knex('users')
-            .where({ email })
-            .first();
-
-          if (getEmail) {
-            return res.status(400).json(errors.userExists);
-          }
-        }
-
-        const updatedUser = await usersTemplate.updateUser(id, name, email, password, cpf, phone);
-
-        if (!updatedUser) {
-          return res.status(400).json(errors.userUpdate);
-        }
-
-        return res.status(204).json(messages.userUpdate);
-      } catch (error) {
+ 
+        return res.status(201).json();
+    } catch (error) {
         return res.status(400).json(error.message);
-      }
-    };
+    }
+};
+*/
 
-    const deleteUser = async (req, res) => {
-      const userLogin = req.user;
 
-      try {
-        const user = await knex('users')
-          .select('id', 'name', 'email', 'cpf', 'phone', 'password')
-          .where({ id: userLogin.id }).first();
+const updateUser = async (req, res) => {
+  const { id } = req.user;
+  let { name, email, password, cpf, phone } = req.body;
 
-        if (!user) {
-          return res.status(404).json(errors.userNotFound);
-        }
+  try {
+    await userSchema.editUser.validate(req.body);
+    const getUser = await usersTemplate.userExists(id);
+    if (!getUser) {
+      return res.status(404).json(errors.userNotFound);
+    }
 
-        const deletedUser = await knex('users')
-          .del()
-          .where({ id: userLogin.id });
+    if (password) {
+      password = await bcrypt.hash(password.trim(), 10);
+    }
 
-        if (!deletedUser) {
-          return res.status(400).json(errors.userDelete);
-        }
+    if (email && email !== req.user.email) {
+      const getEmail = await knex('users')
+        .where({ email })
+        .first();
 
-        return res.status(204).json();
-      } catch (error) {
-        return res.status(400).json(error.message);
+      if (getEmail) {
+        return res.status(400).json(errors.userExists);
       }
     }
 
-    module.exports = {
-      registerUser,
-      informationToTheUserHimself,
-      updateUser,
-      deleteUser
-    };
+    const updatedUser = await usersTemplate.updateUser(id, name, email, password, cpf, phone);
+
+    if (!updatedUser) {
+      return res.status(400).json(errors.userUpdate);
+    }
+
+    return res.status(204).json(messages.userUpdate);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userLogin = req.user;
+
+  try {
+    const user = await knex('users')
+      .select('id', 'name', 'email', 'cpf', 'phone', 'password')
+      .where({ id: userLogin.id }).first();
+
+    if (!user) {
+      return res.status(404).json(errors.userNotFound);
+    }
+
+    const deletedUser = await knex('users')
+      .del()
+      .where({ id: userLogin.id });
+
+    if (!deletedUser) {
+      return res.status(400).json(errors.userDelete);
+    }
+
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = {
+  registerUser,
+  informationToTheUserHimself,
+  updateUser,
+  deleteUser
+}
